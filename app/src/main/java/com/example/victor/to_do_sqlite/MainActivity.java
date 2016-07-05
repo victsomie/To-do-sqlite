1ae88e1f27f2eb5c019fc9d38ea90c0d88c7b6b1
victsomie <victsomie@yahoo.com>
2016-07-05 21:57:30 +0300

    added dialog for data entery

--- app/src/main/java/com/example/victor/to_do_sqlite/MainActivity.java
+++ app/src/main/java/com/example/victor/to_do_sqlite/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.victor.to_do_sqlite;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity"; //This is just a tag
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +48,26 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add_task) {
+            //Log.d(TAG, "Add a new task"); //This logs to the console
+
+            //On clicking the action task button
+            //Make a dialog appear
+            final EditText taskEditText = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Add a new task")
+                    .setMessage("What do you want to do next?")
+                    .setView(taskEditText)
+                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String task = String.valueOf(taskEditText.getText());
+                            Log.d(TAG, "Task to add: " + task);
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create();
+            dialog.show();
             return true;
         }
 
